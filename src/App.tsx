c7f4a1b432b6eadb23ed27a6a3f0760c8c047eb1
Avinash Joshi <avinash.joshi@selu.edu>
2021-07-21 23:05:19 -0500

    List functional

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,30 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 import List from "./components/List";
+import "./App.css";
 
-interface IState  {
+interface IState {
   people: {
-    name: string,
-    age: number,
-    url: string,
-    note?: string
-  }[]
+    name: string;
+    age: number;
+    url: string;
+    note?: string;
+  }[];
 }
 
 function App() {
-
-  const [people, setPeople] = useState<IState["people"]>([])
-
+  const [people, setPeople] = useState<IState["people"]>([
+    {
+      name: "Lebron James",
+      url: "https://cdn.nba.com/headshots/nba/latest/1040x760/2544.png",
+      age: 32,
+      note: "Allergic to something"
+    }
+  ]);
 
   return (
     <div className="App">
       <h1>People Invited to my Party</h1>
-      <List people= {people}/>
+      <List people={people} />
     </div>
   );
 }
